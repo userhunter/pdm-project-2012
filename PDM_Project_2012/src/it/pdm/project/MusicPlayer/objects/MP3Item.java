e961c752105a67ec444f183984d6c7631df14bda
duna288 <duna288@cafc9fbb-099a-f356-dc88-4dd94ff2d2f6>
2012-05-31 20:49:05 +0000

    mp3 ID3 mapping

--- PDM_Project_2012/src/it/pdm/project/MusicPlayer/objects/MP3Item.java
+++ PDM_Project_2012/src/it/pdm/project/MusicPlayer/objects/MP3Item.java
@@ -1,12 +1,49 @@
 package it.pdm.project.MusicPlayer.objects;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagException;
+import org.jaudiotagger.tag.images.Artwork;
+
 public class MP3Item {
 	private String m_strPath;
 	private String m_strFileName;
+	private Hashtable<String, String> m_htID3Fields = new Hashtable<String, String>();
+	
 	
 	public MP3Item(String strPath, String strFileName) {
 		this.setPath(strPath);
 		this.setFileName(strFileName);
+		
+		File tempFile = new File(getPath()+getFileName());
+		AudioFile f = null;
+		try {
+			f = AudioFileIO.read(tempFile);
+			} 	catch (CannotReadException e) {e.printStackTrace();}
+				catch (IOException e) {e.printStackTrace();}
+				catch (TagException e) {e.printStackTrace();}
+				catch (ReadOnlyFileException e) {e.printStackTrace();}
+				catch (InvalidAudioFrameException e) {e.printStackTrace();}
+		
+		Tag tag = f.getTag();
+		
+		setLocalID3Field("TITLE", tag.getFirst(FieldKey.TITLE));
+		setLocalID3Field("ALBUM", tag.getFirst(FieldKey.ALBUM));
+		setLocalID3Field("ARTIST", tag.getFirst(FieldKey.ARTIST));
+		setLocalID3Field("YEAR", tag.getFirst(FieldKey.YEAR));
+		setLocalID3Field("LENGTH", ""+f.getAudioHeader().getTrackLength());
+		setLocalID3Field("BITRATE", ""+f.getAudioHeader().getBitRate());
+		
+
 	}
 
 	public String getPath() {
@@ -23,5 +60,29 @@ public class MP3Item {
 
 	public void setFileName(String m_strFileName) {
 		this.m_strFileName = m_strFileName;
+	}
+	
+	public void setLocalID3Field(String strFieldName, String strValue){
+		m_htID3Fields.put(strFieldName, strValue);
+	}
+	
+	public String getLocalID3Field(String strFieldName){
+		return m_htID3Fields.get(strFieldName);
+	}
+	
+	public Artwork getCover(){
+		File tempFile = new File(getPath()+getFileName());
+		AudioFile f = null;
+		try {
+			f = AudioFileIO.read(tempFile);
+			} 	catch (CannotReadException e) {e.printStackTrace();}
+				catch (IOException e) {e.printStackTrace();}
+				catch (TagException e) {e.printStackTrace();}
+				catch (ReadOnlyFileException e) {e.printStackTrace();}
+				catch (InvalidAudioFrameException e) {e.printStackTrace();}
+		
+		Tag tag = f.getTag();
+		Artwork coverArt = tag.getFirstArtwork();
+		return coverArt;
 	}
 }
