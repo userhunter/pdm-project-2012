bc03731169368395cbb340c86eb34107d8945127
duna288 <duna288@cafc9fbb-099a-f356-dc88-4dd94ff2d2f6>
2012-06-02 08:39:24 +0000

    [min] MP3Player structure

--- PDM_Project_2012/src/it/pdm/project/MusicPlayer/utils/MP3Player.java
+++ PDM_Project_2012/src/it/pdm/project/MusicPlayer/utils/MP3Player.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Random;
+import java.util.Stack;
 
 import it.pdm.project.MusicPlayer.objects.MP3Item;
 import it.pdm.project.MusicPlayer.objects.MP3Manager;
@@ -14,7 +15,10 @@ import android.media.MediaPlayer;
 public class MP3Player extends MediaPlayer {
 	private boolean m_bIsReady;
 	private MP3Manager m_mp3Manager; //Manager responsabile del reperimento degli mp3
+	
 	private MP3Item m_mp3CurrentPlaying; //Mp3 in riproduzione
+	private Stack m_nextQueue; //Coda dei brani successivi
+	private MP3Item m_mp3Previous; //Mp3 precedente
 	
 	//Hashtable che conterrˆ le canzoni
 	private Hashtable<String, MP3Item> m_htMp3sSongs;
@@ -23,6 +27,8 @@ public class MP3Player extends MediaPlayer {
 		this.m_bIsReady = false;
 		this.m_mp3Manager = new MP3Manager("/sdcard/Music/");
 		this.m_mp3CurrentPlaying = null;
+		this.m_nextQueue = null;
+		this.m_mp3Previous = null;
 		this.m_htMp3sSongs = this.m_mp3Manager.getMp3sTable();
 	}
 	
