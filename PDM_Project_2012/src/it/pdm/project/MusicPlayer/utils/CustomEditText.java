194b1188487b25808cf27996d60e59c6bd1a18e0
duna288 <duna288@cafc9fbb-099a-f356-dc88-4dd94ff2d2f6>
2012-06-08 10:51:33 +0000

    - ClearButton - fixata query su ACTION_UP - spostato il vecchio 'No items' e tradotto

--- PDM_Project_2012/src/it/pdm/project/MusicPlayer/utils/CustomEditText.java
+++ PDM_Project_2012/src/it/pdm/project/MusicPlayer/utils/CustomEditText.java
@@ -1,5 +1,6 @@
 package it.pdm.project.MusicPlayer.utils;
 
+import it.pdm.project.MusicPlayer.R;
 import android.content.Context;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
@@ -7,10 +8,14 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.EditText;
 
+/*
+ * CustomEditText estende EditText aggiungendo la possibilitˆ
+ * di inserire un clear button (drawable) che cancelli il testo scritto
+ */
 public class CustomEditText extends EditText
 {
-	private Drawable dRight;
-	private Rect rBounds;
+	private Drawable m_dRight;
+	private Rect m_rBounds;
 
 	public CustomEditText(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
@@ -23,33 +28,40 @@ public class CustomEditText extends EditText
 	}
 
 	@Override
-	public void setCompoundDrawables(Drawable left, Drawable top,
-			Drawable right, Drawable bottom)
+	public void setCompoundDrawables(Drawable left, Drawable top, Drawable right, Drawable bottom)
 	{
-		if(right !=null)
-		{
-			dRight = right;
+		if(right != null){
+			m_dRight = right;
 		}
 		super.setCompoundDrawables(left, top, right, bottom);
 	}
+	
+	@Override
+	public void setCompoundDrawablesWithIntrinsicBounds(Drawable left, Drawable top, Drawable right, Drawable bottom)
+	{
+		if(right != null){
+			m_dRight = right;
+		}
+		super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+	}
 
+	//Intercettiamo il touch sul nostro clear button. Non essendo un button ma un drawable
+	//	andiamo a controllare le coordinate dell'evento touch e le confrontiamo con la posizione
+	//	e la dimensione del nostro drawable, quindi svuotiamo (clear) il testo
 	@Override
 	public boolean onTouchEvent(MotionEvent event)
 	{
-
-		if(event.getAction() == MotionEvent.ACTION_UP && dRight!=null)
+		if(event.getAction() == MotionEvent.ACTION_UP && m_dRight!=null)
 		{
-			rBounds = dRight.getBounds();
+			m_rBounds = m_dRight.getBounds();
 			final int x = (int)event.getX();
 			final int y = (int)event.getY();
-			//System.out.println("x:/y: "+x+"/"+y);
-			//System.out.println("bounds: "+bounds.left+"/"+bounds.right+"/"+bounds.top+"/"+bounds.bottom);
-			//check to make sure the touch event was within the bounds of the drawable
-			if(x>=(this.getRight()-rBounds.width()) && x<=(this.getRight()-this.getPaddingRight())
-					&& y>=this.getPaddingTop() && y<=(this.getHeight()-this.getPaddingBottom()))
+			if(x >= (this.getRight() - m_rBounds.width()) && x <= (this.getRight() - this.getPaddingRight())
+					&& y >= this.getPaddingTop() && y <= (this.getHeight() - this.getPaddingBottom()))
 			{
-				//System.out.println("touch");
 				this.setText("");
+				/* nascondo il clear button, lo rimostrer˜ dall'Activity */
+				this.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
 				event.setAction(MotionEvent.ACTION_CANCEL);//use this to prevent the keyboard from coming up
 			}
 		}
@@ -59,8 +71,8 @@ public class CustomEditText extends EditText
 	@Override
 	protected void finalize() throws Throwable
 	{
-		dRight = null;
-		rBounds = null;
+		m_dRight = null;
+		m_rBounds = null;
 		super.finalize();
 	}
 }
