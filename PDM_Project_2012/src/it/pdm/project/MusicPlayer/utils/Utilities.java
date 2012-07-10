1cfc372e9d534d3f0c2b6eae73ba2ca75c8ebb55
duna288 <duna288@cafc9fbb-099a-f356-dc88-4dd94ff2d2f6>
2012-07-10 20:44:53 +0000

    aggiunto javadoc

--- PDM_Project_2012/src/it/pdm/project/MusicPlayer/utils/Utilities.java
+++ PDM_Project_2012/src/it/pdm/project/MusicPlayer/utils/Utilities.java
@@ -2,7 +2,11 @@ package it.pdm.project.MusicPlayer.utils;
 
 public class Utilities {
 	
-	//Conversione del tempo da millisecondi a Stringa formattata tipo hh:mm:ss o mm:ss
+	/**
+	 * Formatta il tempo da millisecondi ad una Stringa del tipo hh:mm:ss o mm:ss
+	 * @param milliseconds Tempo in millisecondi
+	 * @return La Stringa formattata
+	 */
 	public String milliSecondsToTimer(int milliseconds){
         String finalTimerString = "";
         String secondsString = "";
@@ -28,7 +32,12 @@ public class Utilities {
         return finalTimerString;
     }
  
-	//Metodo ausiliario per calcolare la percentuale a partire da valore corrente e valore massimo
+	/**
+	 * Metodo ausiliario per calcolare la percentuale a partire da valore corrente e valore massimo
+	 * @param currentDuration Valore corrente
+	 * @param totalDuration Valore massimo
+	 * @return La percentuale in base ai valori passati
+	 */
 	public int getProgressPercentage(int currentDuration, int totalDuration){
         Double percentage = (double) 0;
  
@@ -42,7 +51,12 @@ public class Utilities {
         return percentage.intValue();
     }
 
-	//Metodo ausiliario per calcolare la posizione corrente a partire dalla percentuale e dal valore massimo
+	/**
+	 * Metodo ausiliario per calcolare la posizione corrente a partire dalla percentuale e dal valore massimo
+	 * @param progress Percentuale di progresso
+	 * @param totalDuration Durata totale
+	 * @return Posizione corrente di esecuzione
+	 */
     public static int progressToTimer(int progress, int totalDuration) {
         int currentDuration = 0;
         totalDuration = (int) (totalDuration / 1000);
